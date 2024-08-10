d03a96ead7408d74d085f8e4008f265b90e7b83b
dmptmkn <dm.ptmkn@yandex.ru>
2024-08-10 14:26:45 +0300

    Initial commit

---
+++ src/main/java/org/example/servlets/GreetingServlet.java
@@ -0,0 +1,18 @@
+package org.example.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class GreetingServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String greeting = name == null ? "Hello World!" : "Hello " + name + "!";
+
+        resp.getWriter().write(greeting);
+    }
+}
