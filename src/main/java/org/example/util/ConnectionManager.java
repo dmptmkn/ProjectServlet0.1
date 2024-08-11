9c6a370f4b57888198a6f8914a0a42636acf4c40
dmptmkn <dm.ptmkn@yandex.ru>
2024-08-11 21:50:02 +0300

    add ConnectionManager class

---
+++ src/main/java/org/example/util/ConnectionManager.java
@@ -0,0 +1,24 @@
+package org.example.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ConnectionManager {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/top";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
