f254fec59390846ff22ecb8ec41deb89371b0b5b
dmptmkn <dm.ptmkn@yandex.ru>
2024-08-11 23:15:06 +0300

    add constructors

--- src/main/java/org/example/entity/User.java
+++ src/main/java/org/example/entity/User.java
@@ -1,15 +1,19 @@
 package org.example.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class User {
 
     private Long id;
     private String name;
     private String lastName;
-    private int age;
+    private Integer age;
 
 }
