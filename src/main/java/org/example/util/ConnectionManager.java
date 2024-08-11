8bb8dbabdb1e3692a471a3f5d4af559d57ee1739
dmptmkn <dm.ptmkn@yandex.ru>
2024-08-11 23:14:09 +0300

    minor fix

--- src/main/java/org/example/util/ConnectionManager.java
+++ src/main/java/org/example/util/ConnectionManager.java
@@ -2,23 +2,20 @@ package org.example.util;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ConnectionManager {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/top";
+    private static final String URL = "jdbc:mysql://localhost:3306/test";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
+    @SneakyThrows
     public static Connection getConnection() {
-        try {
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
 }
